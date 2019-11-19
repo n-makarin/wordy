ea620feea08ebbaa1df28e06aa75da5a9fac8080
Nikita Makarin <n-makarin@mail.ru>
2019-11-19 13:56:03 +0300

    Create components/layout home & greeting

---
+++ components/layout/greeting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="greeting">
+    <h1>Welcome to Wordy!</h1>
+    <p>Learn words, create your own word sets, choose appropriate translations, stay free.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/layout/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
