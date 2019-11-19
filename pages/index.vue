85629bd29468a1239a5fc3cfa31e5b13f6d4918e
Nikita Makarin <n-makarin@mail.ru>
2019-11-19 13:56:34 +0300

    Add home & greeting comps to pages/index

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="home">
-    <h1>Welcome to Wordy!</h1>
-    <p>Learn words, create your own word sets, choose appropriate translations, stay free.</p>
+  <div class="index">
+    <home v-if="authorized" />
+    <greeting v-else />
   </div>
 </template>
 
 <script>
+import Greeting from '~/components/layout/greeting'
+import Home from '~/components/layout/home'
 
 export default {
   components: {
+    Greeting,
+    Home
+  },
+  computed: {
+  },
+  asyncData (context) {
+    const authorized = context.store.getters['auth/authorized']
+    return {
+      authorized
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.home {
-}
 </style>
