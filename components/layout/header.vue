6a8442bc1a957ba0e7c015851e9d9431e28366b2
Nikita Makarin <n-makarin@mail.ru>
2019-11-19 12:44:15 +0300

    Create components\layout\header.vue

---
+++ components/layout/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="header">
+    <nuxt-link to="login">
+      login
+    </nuxt-link>
+    <nuxt-link to="register">
+      register
+    </nuxt-link>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
