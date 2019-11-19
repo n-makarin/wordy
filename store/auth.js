82eb3e415088b010c63885a08f143d621b284ad0
Nikita Makarin <n-makarin@mail.ru>
2019-11-19 16:05:22 +0300

    Add prolongAuth action to store\auth.js Add methods descriptions

--- store/auth.js
+++ store/auth.js
@@ -12,9 +12,23 @@ export const mutations = {
 }
 
 export const actions = {
+  /**
+   * Remove user from store and cookies
+   * @returns void
+   */
   logout ({ commit }) {
     commit('SET_USER', null)
     this.app.$cookies.remove(cookies.authUser.name)
+  },
+  /**
+   * Prolong autharization when got user from cookies
+   * @param {object} user User data
+   * @returns void
+   */
+  prolongAuth ({ commit }, user) {
+    this.app.$cookies.set(cookies.authUser.name, user, {
+      maxAge: cookies.authUser.maxAge
+    })
   }
 }
 
