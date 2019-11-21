067474c015dd15b32d411265fe5c705739438421
Nikita Makarin <n-makarin@mail.ru>
2019-11-21 13:07:21 +0300

    Create components\form\item\input.vue

---
+++ components/form/item/input.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="form-item-input">
+    <input
+      v-model="localValue"
+      :id="id"
+      :type="type"
+      :min="range.min"
+      :max="range.max"
+      :placeholder="placeholder"
+      @keydown="$emit('keydown', localValue)"
+      @keyup="$emit('keyup', localValue)"
+    >
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    range: {
+      type: Object,
+      default: () => {
+        return {
+          min: '',
+          max: ''
+        }
+      }
+    }
+  },
+  data () {
+    return {
+      localValue: this.value
+    }
+  },
+  watch: {
+    localValue (newValue) {
+      this.$emit('update', newValue)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
