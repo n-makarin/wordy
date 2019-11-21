4c7cddf1ef9510e0c4e554b827562dcfa46a922b
Nikita Makarin <n-makarin@mail.ru>
2019-11-21 14:22:52 +0300

    Add login method to store\auth.js

--- store/auth.js
+++ store/auth.js
@@ -1,4 +1,5 @@
 import { cookies } from '~/utils/cookies/config.js'
+import { email } from '~/utils/regexp.js'
 
 export const state = () => ({
   authorized: false,
@@ -8,10 +9,38 @@ export const state = () => ({
 export const mutations = {
   SET_USER (state, user) {
     state.user = user
+    state.authorized = !!user
   }
 }
 
 export const actions = {
+  /**
+   * Try to login and add user to store and cookies
+   * @returns void
+   */
+  async login ({ commit }, { login = null, password = null }) {
+    let url = ''
+    if (email.test(login)) {
+      url = `/login-email/?email=${login}&password=${password}`
+    } else {
+      url = `/login/?login=${login}&password=${password}`
+    }
+    await this.app.$sendRequest({
+      method: 'GET',
+      url
+    })
+      .catch((err) => {
+        console.log(err)
+      })
+      .then((response) => {
+        if (!response || !response.data || response.data.length === 0) { return }
+        const user = response.data[0]
+        commit('SET_USER', user)
+        this.app.$cookies.set(cookies.authUser.name, user, {
+          maxAge: cookies.authUser.maxAge
+        })
+      })
+  },
   /**
    * Remove user from store and cookies
    * @returns void
