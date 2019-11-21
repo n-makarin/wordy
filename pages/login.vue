e161fa04a419b75121d8f5bebba3ef91ffe04fd3
Nikita Makarin <n-makarin@mail.ru>
2019-11-21 13:21:21 +0300

    Add form-default compt to pages/login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,72 @@
 <template>
   <div class="login">
-    login
+    <h1>Login</h1>
+    <form-default
+      :field-list="form.fieldList"
+      :button-list="form.buttonList"
+      @update="update"
+      @submit="submit"
+    />
   </div>
 </template>
 
 <script>
+import FormDefault from '~/components/form/default'
 
 export default {
   components: {
+    FormDefault
+  },
+  data () {
+    return {
+      form: {
+        fieldList: [
+          {
+            id: 'login',
+            value: '',
+            label: 'Username or email address',
+            field: 'input',
+            type: 'text',
+            required: true,
+            range: {
+              min: 4,
+              max: 20
+            }
+          },
+          {
+            id: 'password',
+            value: '',
+            label: 'Password',
+            field: 'input',
+            type: 'password',
+            required: true,
+            range: {
+              min: 6,
+              max: 30
+            }
+          }
+        ],
+        buttonList: [
+          {
+            id: 'submit',
+            value: 'submit'
+          }
+        ]
+      }
+    }
   },
   computed: {
   },
   asyncData (context) {
+  },
+  methods: {
+    update (fieldList) {
+      this.form.fieldList = fieldList
+    },
+    submit () {
+      console.log(this.form.fieldList)
+      console.log('submitted')
+    }
   }
 }
 </script>
