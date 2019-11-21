b468bfb99dceef894df8a2c2950b89988c35e52c
Nikita Makarin <n-makarin@mail.ru>
2019-11-21 13:07:40 +0300

    Create components\form\item\textarea.vue

---
+++ components/form/item/textarea.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="form-item-textarea">
+    <textarea
+      v-model="localValue"
+      :id="id"
+      :cols="cols"
+      :rows="rows"
+      :min="min"
+      :max="max"
+      :placeholder="placeholder"
+      @keydown="$emit('keydown', localValue)"
+      @keyup="$emit('keyup', localValue)"
+    />
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    range: {
+      type: Object,
+      default: () => {
+        return {
+          min: '',
+          max: ''
+        }
+      }
+    },
+    cols: {
+      type: null,
+      default: ''
+    },
+    rows: {
+      type: null,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      localValue: this.value
+    }
+  },
+  watch: {
+    localValue (newValue) {
+      this.$emit('update', newValue)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
