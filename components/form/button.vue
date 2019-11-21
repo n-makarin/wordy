e27c8c60e55e93a55df93bfd368c360699268c83
Nikita Makarin <n-makarin@mail.ru>
2019-11-21 13:05:39 +0300

    Create components\form\button.vue

---
+++ components/form/button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    :type="type || 'submit'"
+    @click="click"
+    class="form-button"
+  >
+    {{ value }}
+  </button>
+</template>
+
+<script>
+
+export default {
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    onclick: {
+      type: Function,
+      default: () => {}
+    }
+  },
+  computed: {
+    /**
+     * Action may be submit nor reset
+     */
+    click () {
+      return this.onclick ? this.onclick : function () {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
