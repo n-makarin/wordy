cbdb5e6177e50ed564ecca6280e05a70207ea54e
Nikita Makarin <n-makarin@mail.ru>
2019-11-21 13:09:07 +0300

    Create components\form\item\label.vue

---
+++ components/form/item/label.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="form-item-label">
+    <label
+      :class="className"
+      :for="id"
+    >
+      {{ value }}
+    </label>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: String,
+      default: ''
+    },
+    required: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    className () {
+      const main = 'form-item__label'
+      const modifier = {
+        required: '_required'
+      }
+      return {
+        main,
+        [main + modifier.required]: this.required
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
