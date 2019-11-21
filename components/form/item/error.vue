b5ba14bc4d209e0e025ea0415f9829fcdd84e6bf
Nikita Makarin <n-makarin@mail.ru>
2019-11-21 13:05:52 +0300

    Create components\form\item\error.vue

---
+++ components/form/item/error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    v-if="error && error.visible"
+    class="form-item-error"
+  >
+    {{ error.message }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {
+        return {
+          visible: false,
+          message: ''
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
