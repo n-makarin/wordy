20aeb1d0d91f872ae623d3f1b295f35ccf614452
Nikita Makarin <n-makarin@mail.ru>
2019-11-28 15:53:19 +0300

    Create components\layout\header\logo.vue Add it to header.vue

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,18 +1,21 @@
 <template>
   <header class="header">
     <auth :authorized="authorized" />
+    <logo />
     <locale />
   </header>
 </template>
 
 <script>
 import Auth from '~/components/layout/header/auth'
+import Logo from '~/components/layout/header/logo'
 import Locale from '~/components/layout/header/locale'
 
 export default {
   components: {
     Auth,
-    Locale
+    Locale,
+    Logo
   },
   computed: {
     authorized () {
@@ -26,6 +29,7 @@ export default {
 .header {
   display: flex;
   justify-content: space-around;
+  align-items: center;
   border-bottom: 1px solid rgb(197, 197, 197);
   padding: 5px 0;
 }
---
+++ components/layout/header/logo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="logo">
+    <nuxt-link to="/">
+      Wordy
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  a {
+    text-decoration: none;
+    color: #304455;
+    font-weight: 500;
+  }
+}
+</style>
