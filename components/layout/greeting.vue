4da1a28dabaca24a2fa9226c6ce2329174e7a717
Nikita Makarin <n-makarin@mail.ru>
2019-11-28 16:31:26 +0300

    Add auth.vue to components\layout\greeting.vue

--- components/layout/greeting.vue
+++ components/layout/greeting.vue
@@ -2,13 +2,16 @@
   <div class="greeting">
     <h1>Welcome to Wordy!</h1>
     <p>Learn words, create your own word sets, choose appropriate translations, stay free.</p>
+    <auth />
   </div>
 </template>
 
 <script>
+import Auth from '~/components/layout/header/menu/auth'
 
 export default {
   components: {
+    Auth
   }
 }
 </script>
