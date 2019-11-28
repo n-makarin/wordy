4c922f5970b5b786c087a492a3ebad6d0a206208
Nikita Makarin <n-makarin@mail.ru>
2019-11-28 17:17:58 +0300

    Crate components\layout\header\menu\link-list.vue

---
+++ components/layout/header/menu/link-list.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="link-list">
+    link list
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
