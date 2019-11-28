aa1f891a9a4597d121ccc41dd2b34e2b742475ba
Nikita Makarin <n-makarin@mail.ru>
2019-11-28 17:17:50 +0300

    Create components\layout\header\menu\content.vue

---
+++ components/layout/header/menu/content.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :style="styleList" class="content">
+    <slot />
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    // headerHeight: {
+    //   type: Number,
+    //   default: 0
+    // }
+  },
+  data () {
+    return {
+      headerHeight: 0
+    }
+  },
+  computed: {
+    styleList () {
+      return {
+        top: this.headerHeight + 'px'
+      }
+    }
+  },
+  beforeMount () {
+    debugger
+    this.headerHeight = this.$parent.$parent.$el.offsetHeight
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  position: fixed;
+  right: 0;
+  width: 200px;
+  height: 100%;
+  border-left: 1px solid rgb(182, 182, 182);
+}
+</style>
