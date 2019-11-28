554d543a492642902579bf6aa8b45435ffe8d4a6
Nikita Makarin <n-makarin@mail.ru>
2019-11-28 16:30:47 +0300

    Create components\layout\header\menu.vue

---
+++ components/layout/header/menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="menu">
+    <locale />
+    <auth />
+  </div>
+</template>
+
+<script>
+import Auth from '~/components/layout/header/menu/auth'
+import Locale from '~/components/layout/header/menu/locale'
+
+export default {
+  components: {
+    Auth,
+    Locale
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
