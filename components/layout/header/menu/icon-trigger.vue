b35559955a1c9e1a8e39f052f79aedf8b239c4a1
Nikita Makarin <n-makarin@mail.ru>
2019-11-28 17:17:41 +0300

    Create \layout\header\menu\icon-trigger.vue

---
+++ components/layout/header/menu/icon-trigger.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="icon-trigger">
+    icon
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
