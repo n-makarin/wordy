dac85dd805774023c6571f5c968982a8dd4e7f03
Nikita Makarin <n-makarin@mail.ru>
2019-11-29 14:02:32 +0300

    Create pages scaffolds: about, profile, settings

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    about
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="profile">
+    profile
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
