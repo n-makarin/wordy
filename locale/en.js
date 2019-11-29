fed54b271cdb1d9fcbf743405508cc8a5c7a7590
Nikita Makarin <n-makarin@mail.ru>
2019-11-29 13:33:26 +0300

    Add components/layout/header/menu locales

--- locale/en.js
+++ locale/en.js
@@ -1,3 +1,5 @@
 export * from '~/locale/components/layout/en-home.json'
+export * from '~/locale/components/layout/header/en-menu.json'
+
 export * from '~/locale/pages/en-login.json'
 export * from '~/locale/pages/en-register.json'
--- locale/ru.js
+++ locale/ru.js
@@ -1,3 +1,5 @@
 export * from '~/locale/components/layout/ru-home.json'
+export * from '~/locale/components/layout/header/ru-menu.json'
+
 export * from '~/locale/pages/ru-login.json'
 export * from '~/locale/pages/ru-register.json'
